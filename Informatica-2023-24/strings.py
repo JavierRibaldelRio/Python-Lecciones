fbdb290df8f3bd321c8a82907ff6425597178d1c
Javier Ribal del Río <javierribal@gmail.com>
2023-10-24 07:50:07 +0000

    Strings

---
+++ Informatica-2023-24/strings.py
@@ -0,0 +1,12 @@
+pa1 = "Informática"
+pa2= "programación"
+
+print(pa1+pa2)
+print(pa1[0]+pa2[2])
+print(pa2[6:])
+print(pa1 +'---'+pa2)
+print(pa1[:4])
+print(pa1[:])
+print(pa1[3])
+print(pa2[-4])
+print(list(pa1))
